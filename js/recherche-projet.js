1d8810d58999f447ddddc88f8ffcfc40b7c7cbd5
Léa Garaix <lea.garaix@etu.univ-grenoble-alpes.fr>
2025-04-27 17:40:42 +0200

    Ajout de projets, corrections, interfaces.

---
+++ js/recherche-projet.js
@@ -0,0 +1,12 @@
+/* const barreRecherche = document.getElementById("barre-recherche-projets");
+const buttonRecherche = document.getElementById("button-recherche");
+
+barreRecherche.addEventListener("keydown", handleRecherche);
+buttonRecherche.addEventListener("click", handleRecherche);
+
+function handleRecherche() {
+    console.log("Click ou Enter");
+
+
+
+} */
